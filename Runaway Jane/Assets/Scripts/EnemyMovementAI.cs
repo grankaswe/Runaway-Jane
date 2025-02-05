7b8dc5644728d61598f5cb2efb59ae73ee3a17b1
Elias Damavandi Jahromi <117083631+grankaswe@users.noreply.github.com>
2025-02-05 14:55:38 +0100

    enemy movement

---
+++ Runaway Jane/Assets/Scripts/EnemyMovementAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+}
