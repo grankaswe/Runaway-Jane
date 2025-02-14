be67bbc23bd2d3fea36953799df916e2e2658048
SiggeBjork <sigge.bjork@elev.ga.lbs.se>
2025-02-14 12:25:56 +0100

    Merge branch 'main' of https://github.com/grankaswe/Runaway-Jane

--- Runaway Jane/Assets/Scripts/Items&Interact/HidingSpot.cs
+++ Runaway Jane/Assets/Scripts/Items&Interact/HidingSpot.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 
 public class HidingSpot : MonoBehaviour
 {
-    private bool playerInside = false; // Is the player in range to hide?
-    private bool isHiding = false; // Is the player currently hiding?
+    private bool playerInside = false;  // Is the player inside the hiding spot?
+    private bool isHiding = false;  // Is the player hiding?
     private GameObject player;
     private SpriteRenderer playerSprite;
     private Camera mainCamera;
 
-    [SerializeField] private Vector3 cameraOffset = new Vector3(0, 0, -10); // Camera position offset
-    [SerializeField] private KeyCode hideKey = KeyCode.E; // Key to hide/unhide
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0, 0, -10);  // Camera position offset when hiding
+    [SerializeField] private KeyCode hideKey = KeyCode.E;  // Key to hide/unhide
 
-    public bool IsHiding => isHiding; // Property to check if the player is hiding
+    public bool IsHiding => isHiding;  // Public property to check if the player is hiding
 
     private void Start()
     {
-        mainCamera = Camera.main; // Get the main camera
+        mainCamera = Camera.main;  // Get the main camera
     }
 
     private void Update()
     {
-        if (playerInside && Input.GetKeyDown(hideKey))
+        if (playerInside && Input.GetKeyDown(hideKey))  // If player is inside and presses the hide key
         {
             if (!isHiding)
             {
@@ -38,30 +38,30 @@ public class HidingSpot : MonoBehaviour
         if (player == null) return;
 
         isHiding = true;
-        playerSprite.enabled = false; // Hide player sprite
-        mainCamera.transform.position = transform.position + cameraOffset; // Center camera on hiding spot
+        playerSprite.enabled = false;  // Hide player sprite
+        mainCamera.transform.position = transform.position + cameraOffset;  // Move camera to hiding spot
     }
 
     private void ExitHiding()
     {
         isHiding = false;
-        playerSprite.enabled = true; // Show player sprite
-        mainCamera.transform.position = player.transform.position + cameraOffset; // Return camera to player
+        playerSprite.enabled = true;  // Show player sprite
+        mainCamera.transform.position = player.transform.position + cameraOffset;  // Return camera to player
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))  // If the player collides with the hiding spot
         {
             playerInside = true;
             player = collision.gameObject;
-            playerSprite = player.GetComponent<SpriteRenderer>();
+            playerSprite = player.GetComponent<SpriteRenderer>();  // Get the player's SpriteRenderer
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))  // If the player leaves the hiding spot
         {
             playerInside = false;
 
